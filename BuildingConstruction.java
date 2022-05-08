d0fe6d4076aa3c41ed262c5160b652ed3d1f9267
Shreyas02Bane <shreyash77bane@gmail.com>
2022-05-08 21:01:50 +0530

    Graded Project 3 final commit

--- BuildingConstruction.java
+++ BuildingConstruction.java
@@ -3,7 +3,9 @@
  */
 package com.greatLearning.gradedProject2;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * @author Shreyas
@@ -14,61 +16,118 @@ public class BuildingConstruction {
 	/**
 	 * @param args
 	 */
-	
-	int floors;
-	int arr[];
-	
-	Scanner sc = new Scanner(System.in);
-	
-	public void noOfFloors() {
-		System.out.println("Enter the number of floors for the building : ");
-		floors = sc.nextInt();
-		arr = new int[floors];
-	}
-	
-	public void floorPlan() {
-		noOfFloors();
-		for(int i = 0; i < floors; i++) {
-			System.out.println("Enter the floor size on Day "+(i+1));
-			arr[i] = sc.nextInt();
-		}
-		int count = 1;
-		int maxOfArr = getMaxFromArr();
-		for(int i = 0; i < floors; i++) {
-			System.out.println("Day "+count);
-			if(arr[i] == maxOfArr) {
-				int k = i;
-				while(k >= 0 && arr[k] == maxOfArr) {
-					System.out.print(arr[k]+" ");
-					arr[k] = 0;
-					maxOfArr = getMaxFromArr();
-					k--;
+
+	static boolean largestNumber;
+	static boolean addFlag;
+
+	@SuppressWarnings("unchecked")
+	public static void calculateFloors(int[] floorSize) {
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = 0; i < floorSize.length; i++) {
+			largestNumber = true;
+			int floorSize_i = floorSize[i];
+			for (int j = i; j < floorSize.length; j++) {
+				if (j == i) {
+					continue;
+				} else {
+					int floorSize_j = floorSize[j];
+					if (floorSize_i >= floorSize_j) {
+						largestNumber = true;
+						continue;
+					} else {
+						largestNumber = false;
+						stack.push(floorSize_i);
+						break;
+					}
 				}
-				System.out.println();
-			}else {
-				System.out.println();
 			}
-			count++;
+
+			if (largestNumber == true) {
+				try {
+					Stack<Integer> stackTmp = displayFloor(stack, floorSize_i, i, floorSize);
+					stack.clear();
+					stack = (Stack<Integer>) stackTmp.clone();
+					stackTmp.clear();
+					System.out.println();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				System.out.println("Day " + (i + 1) + " :");
+			}
 		}
 	}
-	
-	public int getMaxFromArr() {
-		int max= Integer.MIN_VALUE;
-		for(int k = 0; k < floors; k++) {
-			if(arr[k] > max) {
-				max = arr[k];
+
+	public static Stack<Integer> displayFloor(Stack<Integer> stack, int currentFloor, int iter, int[] arrayFloor) {
+
+		int[] arrayOfPoppedItems = new int[arrayFloor.length];
+		int count = 0;
+
+		System.out.print("Day " + (iter + 1) + " : " + currentFloor);
+		Stack<Integer> stackTmp = new Stack<Integer>();
+		boolean largestInStack;
+		if (iter == arrayFloor.length - 1) {
+			while (!stack.empty()) {
+				int popFloor = (int) stack.pop();
+				arrayOfPoppedItems[count++] = popFloor;
 			}
+			if (arrayOfPoppedItems != null) {
+				Arrays.sort(arrayOfPoppedItems);
+				for (int i = arrayOfPoppedItems.length - 1; i > 0; i--) {
+					if (arrayOfPoppedItems[i] != 0) {
+						System.out.print(" " + arrayOfPoppedItems[i]);
+					}
+
+				}
+			}
+
+			return stackTmp;
+		} else {
+			if (!stack.empty()) {
+				while (!stack.empty()) {
+					largestInStack = true;
+					int popFloor = (int) stack.pop();
+					for (int i = iter; i < arrayFloor.length; i++) {
+						if (i == iter) {
+							continue;
+						}
+						if (popFloor < arrayFloor[i]) {
+							stackTmp.push(popFloor);
+							largestInStack = false;
+							break;
+						} else {
+							largestInStack = true;
+						}
+					}
+					if (largestInStack == true) {
+						arrayOfPoppedItems[count++] = popFloor;
+					}
+				}
+			}
+			if (arrayOfPoppedItems != null) {
+				Arrays.sort(arrayOfPoppedItems);
+				for (int i = arrayOfPoppedItems.length - 1; i > 0; i--) {
+					if (arrayOfPoppedItems[i] != 0) {
+						System.out.print(" " + arrayOfPoppedItems[i]);
+					}
+				}
+			}
+
+			return stackTmp;
 		}
-		return max;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		BuildingConstruction buildPlan =  new BuildingConstruction();
-		buildPlan.floorPlan();
-
+		System.out.println("Enter the total no of floors in the building : ");
+		try (Scanner sc = new Scanner(System.in)) {
+			int numberOfFloors = sc.nextInt();
+			int[] listOfFloors = new int[numberOfFloors];
+			for (int i = 0; i < numberOfFloors; i++) {
+				System.out.println("Enter the floor size given on day : " + (i + 1));
+				int floorSize = sc.nextInt();
+				listOfFloors[i] = floorSize;
+			}
+			BuildingConstruction.calculateFloors(listOfFloors);
+		}
 	}
-
 }
